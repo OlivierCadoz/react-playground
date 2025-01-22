72eac361fc3b8924be8408362e3d24c401800781
OlivierCadoz <cadoz.olivier@gmail.com>
2025-01-22 14:26:48 +0100

    Display numbers left to assign

--- src/domains/Sudoku/components/molecules/NumericPad/NumericPad.tsx
+++ src/domains/Sudoku/components/molecules/NumericPad/NumericPad.tsx
@@ -3,16 +3,20 @@ import ButtonCommon from '@/components/atoms/ButtonCommon';
 import { numerics } from '@/domains/Sudoku/constants/numerics';
 
 export default function NumericPad({
+  numbersLeft,
   onNumberClick,
 }: {
+  numbersLeft: number[];
   onNumberClick: (value: number) => void;
 }) {
   return (
     <ul className="numeric-pad">
-      {numerics.map((number) => (
+      {numerics.map((number, i) => (
         <li key={number}>
           <ButtonCommon onClickEvent={() => onNumberClick(number)}>
-            {number}
+            <span className='numeric-pad__number'>{number}</span>
+
+            {!!number && <span className='numeric-pad__left'>{numbersLeft[i]}</span>}
           </ButtonCommon>
         </li>
       ))}

15fe64c220d692ad9c47cec743d89d1d560e4676
OlivierCadoz <cadoz.olivier@gmail.com>
2025-01-24 15:34:36 +0100

    refactor(hook) Adapt hook from sudoku flattened

--- src/domains/Sudoku/hooks/useCellClass.ts
+++ src/domains/Sudoku/hooks/useCellClass.ts
@@ -3,49 +3,46 @@ import {
   SetCellClassParameters,
   UseCellClassParameters,
 } from '@sudoku/models/cellClass.model';
-import { blocs } from '@sudoku/constants/Indexes';
+import { rows, columns, blocs } from '@sudoku/constants/Indexes';
 
-const isSomeSameIndex = (bloc: number[]) => (index: number) =>
-  bloc.some((idx) => index === idx);
+const isIndexInArray = (indexArray: number[]) => (index: number) =>
+  indexArray.some((idx) => index === idx);
 
-const isSameBloc = (currentIndex: number, itemIndex: number) =>
-  blocs.some(
-    (bloc) =>
-      isSomeSameIndex(bloc)(currentIndex) && isSomeSameIndex(bloc)(itemIndex)
+const isIndexInArrayAsSelected =
+  (cellIndex: number, currentIndex: number) => (indexArray: number[]) => {
+    const isIndexIncluded = isIndexInArray(indexArray);
+    return isIndexIncluded(cellIndex) && isIndexIncluded(currentIndex);
+  };
+
+const shouldHighlight = (cellIndex: number, currentIndex: number) => {
+  const isIndexAlsoIncluded = isIndexInArrayAsSelected(cellIndex, currentIndex);
+  return [rows, columns, blocs].some((indexArrays) =>
+    indexArrays.some(isIndexAlsoIncluded)
   );
+};
 
 export function useCellClass({
   sudoku,
   solution,
-  currentIndexes,
+  currentIndex,
 }: UseCellClassParameters) {
-  const getItemIndex = (rowIndex: number, cellIndex: number) =>
-    rowIndex * 8 + rowIndex + cellIndex;
-
   const setCellClass = useCallback(
-    ({ cell, rowIndex, cellIndex, itemIndex }: SetCellClassParameters) => {
+    ({ cell, cellIndex }: SetCellClassParameters) => {
       const classes = [];
-      const [rowIdx, cellIdx] = currentIndexes;
-      const sameRowIdx = rowIndex === rowIdx;
-      const sameCellIdx = cellIndex === cellIdx;
       const selectedOrSameValueAsSelected =
-        (sameRowIdx && sameCellIdx) ||
-        (cell && sudoku[rowIdx]?.[cellIdx] === cell);
-      const currentItemIndex = getItemIndex(rowIdx, cellIdx);
-      const sameBloc = isSameBloc(currentItemIndex, itemIndex);
+        cellIndex === currentIndex || (cell && sudoku[currentIndex] === cell);
+      const highlight = shouldHighlight(cellIndex, currentIndex);
 
       if (selectedOrSameValueAsSelected) classes.push('sudoku-cell--selected');
-      else if (sameRowIdx || sameCellIdx || sameBloc)
-        classes.push('sudoku-cell--highlighted');
+      else if (highlight) classes.push('sudoku-cell--highlighted');
 
-      const cellWrong =
-        sudoku[rowIndex][cellIndex] !== solution[rowIndex][cellIndex];
+      const cellWrong = sudoku[cellIndex] !== solution[cellIndex];
       if (cellWrong) classes.push('sudoku-cell--error');
 
       return classes.join(' ');
     },
-    [currentIndexes, sudoku]
+    [currentIndex, sudoku]
   );
 
-  return { setCellClass, getItemIndex };
+  return { setCellClass };
 }
--- src/domains/Sudoku/hooks/useNumberLeft.ts
+++ src/domains/Sudoku/hooks/useNumberLeft.ts
@@ -1,21 +1,23 @@
 import { useState, useEffect } from 'react';
 import { SudokuType } from '@sudoku/models/sudoku.model';
 
-export function useNumberLeft(sudoku: SudokuType, solution: SudokuType) {
-  const [numbersLeft, setNumbersLeft] = useState(Array(9).fill(0));
+export function useNumberLeft(
+  sudoku: SudokuType | null,
+  solution: SudokuType | null
+) {
+  const [numbersLeft, setNumbersLeft] = useState<number[]>(Array(9).fill(0));
 
   useEffect(() => {
-    const nbrsLeft = Array(9).fill(0);
+    const nbrsLeft: number[] = Array(9).fill(0);
 
-    sudoku?.forEach((row, rowIndex) =>
-      row.forEach((cell, cellIndex) => {
+    solution &&
+      sudoku?.forEach((cell, cellIndex) => {
         if (!cell) {
-          const solutionCell = solution[rowIndex][cellIndex];
+          const solutionCell = solution[cellIndex];
           const currentIndex = solutionCell - 1;
           nbrsLeft[currentIndex] = nbrsLeft[currentIndex] + 1;
         }
-      })
-    );
+      });
 
     setNumbersLeft(nbrsLeft);
   }, [sudoku, solution]);

1903aaadd4e2cc1205f6f28a7b07afb47df78c8f
OlivierCadoz <cadoz.olivier@gmail.com>
2025-01-24 15:27:18 +0100

    refactor(hook) Flatten sudoku and solution arrays of number array

--- src/domains/Sudoku/hooks/useSudoku.ts
+++ src/domains/Sudoku/hooks/useSudoku.ts
@@ -1,51 +1,39 @@
 import { useState, useEffect } from 'react';
-import {
-  GridIndexesTuple,
-  GridModel,
-  SudokuType,
-} from '@sudoku/models/sudoku.model';
+import { GridModel, SudokuType } from '@sudoku/models/sudoku.model';
 import { deepClone } from '@sudoku/utils';
 
 export function useSudoku(grid: GridModel | null) {
   const [sudoku, setSudoku] = useState<SudokuType | null>(null);
   const [solution, setSolution] = useState<SudokuType | null>(null);
-  const [currentIndexes, setCurrentIndexes] = useState<GridIndexesTuple>([
-    -1, -1,
-  ]);
+  const [currentIndex, setCurrentIndex] = useState<number>(-1);
 
   useEffect(() => {
     if (grid) {
-      setSudoku(deepClone(grid.value));
-      setSolution(deepClone(grid.solution));
+      setSudoku(deepClone(grid.value.flat()));
+      setSolution(deepClone(grid.solution.flat()));
     }
   }, [grid]);
 
-  function handleCellClick(rowIndex: number, cellIndex: number) {
-    setCurrentIndexes([rowIndex, cellIndex]);
+  function handleCellClick(cellIndex: number) {
+    setCurrentIndex(cellIndex);
   }
 
   function handleValueChange(value: number) {
-    const clone: SudokuType = deepClone(sudoku!);
-    const [rowIndex, cellIndex] = currentIndexes;
     const isCurrentCellCorrect =
-      clone[rowIndex][cellIndex] === solution![rowIndex][cellIndex];
-
-    if (isCurrentCellCorrect) return;
+      sudoku?.[currentIndex] === solution?.[currentIndex];
+    if (!sudoku || isCurrentCellCorrect) return;
 
     const cellMap = (cell: number, cellIdx: number) =>
-      cellIdx === cellIndex ? value : cell;
+      cellIdx === currentIndex ? value : cell;
 
-    const rowMap = (row: number[], rowIdx: number) =>
-      rowIdx === rowIndex ? row.map(cellMap) : row;
-
-    const nextSudoku = clone.map(rowMap);
+    const nextSudoku: SudokuType = sudoku.map(cellMap);
     setSudoku(nextSudoku);
   }
 
   return {
     sudoku,
     solution,
-    currentIndexes,
+    currentIndex,
     handleCellClick,
     handleValueChange,
   };

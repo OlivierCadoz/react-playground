c557c94cd502404c05dff35ca20b03df84e70041
OlivierCadoz <cadoz.olivier@gmail.com>
2025-01-24 16:52:30 +0100

    feat(hook) Add logic for winning the game

--- src/domains/Sudoku/hooks/useSudoku.ts
+++ src/domains/Sudoku/hooks/useSudoku.ts
@@ -6,6 +6,7 @@ export function useSudoku(grid: GridModel | null) {
   const [sudoku, setSudoku] = useState<SudokuType | null>(null);
   const [solution, setSolution] = useState<SudokuType | null>(null);
   const [currentIndex, setCurrentIndex] = useState<number>(-1);
+  const [isSolved, setIsSolved] = useState<boolean>(false);
 
   useEffect(() => {
     if (grid) {
@@ -21,19 +22,25 @@ export function useSudoku(grid: GridModel | null) {
   function handleValueChange(value: number) {
     const isCurrentCellCorrect =
       sudoku?.[currentIndex] === solution?.[currentIndex];
-    if (!sudoku || isCurrentCellCorrect) return;
+    if (!sudoku || !solution || isCurrentCellCorrect) return;
 
     const cellMap = (cell: number, cellIdx: number) =>
       cellIdx === currentIndex ? value : cell;
 
     const nextSudoku: SudokuType = sudoku.map(cellMap);
     setSudoku(nextSudoku);
+
+    const isSudokuSolved = nextSudoku.every(
+      (cell, cellIndex) => cell === solution[cellIndex]
+    );
+    isSudokuSolved && setIsSolved(true);
   }
 
   return {
     sudoku,
     solution,
     currentIndex,
+    isSolved,
     handleCellClick,
     handleValueChange,
   };

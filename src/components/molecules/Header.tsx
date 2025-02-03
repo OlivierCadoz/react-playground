b778a2c7b6d7883c5c07d6560a5c8b6b74a8c685
OlivierCadoz <cadoz.olivier@gmail.com>
2025-02-03 14:51:59 +0100

    feat(view) Style the header

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -4,11 +4,13 @@ import { isActive } from '@utils/header';
 import { navLinks } from '@constants/header';
 import { NavLinkModel } from "@models/header.model";
 
+import '@/components/molecules/Header.scss'
+
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
+    <header className="header">
+      <nav className="header__nav">
+        <ul className="header__nav-list">
           {navLinks.map((link: NavLinkModel) => (
             <li key={link.label}>
               <NavLink to={link.path} className={isActive}>
